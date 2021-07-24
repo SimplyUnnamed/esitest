5ad26076e7500635108752cb923985c82e6d0c2d
= <=>
2021-07-24 23:56:14 +1000

    horizon and queue fix

--- config/database.php
+++ config/database.php
@@ -134,14 +134,6 @@ return [
             'database' => env('REDIS_DB', '3'),
         ],
 
-        'db3' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '3'),
-        ],
-
         'cache' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
